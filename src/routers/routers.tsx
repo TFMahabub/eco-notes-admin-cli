03ddfd629e4e6786a450eeedf77d6db178dabe3e
Mahabub Alam <rjmahabub543@gmail.com>
2023-07-19 09:29:38 +0600

    [CHANGE] folder structure

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
-import DashboardLayouts from '../components/layouts/DashboardLayout';
-import Dashboard from '../components/pages/dashboard/Dashboard';
+import DashboardLayouts from '../layouts/DashboardLayout';
+import Dashboard from '../pages/dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
