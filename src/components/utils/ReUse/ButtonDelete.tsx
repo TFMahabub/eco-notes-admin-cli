8982ca2bbcb2544215ac8cd481e466b591394c4e
Mahabub Alam <rjmahabub543@gmail.com>
2023-08-08 06:26:02 +0600

    [MODIFY] create reuseable button and added

--- src/components/utils/ReUse/ButtonDelete.tsx
+++ src/components/utils/ReUse/ButtonDelete.tsx
@@ -8,6 +8,7 @@ type PropsType = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export default function ButtonDelete({ ...attribute }: PropsType) {
   return (
     <button
+      title="Delete"
       {...attribute}
       className="flex items-center justify-center text-md h-8 w-8 rounded-md text-error bg-error/10 cursor-pointer hover:bg-error/20"
     >
--- src/components/utils/ReUse/ButtonWatch.tsx
+++ src/components/utils/ReUse/ButtonWatch.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import { RiEditBoxLine } from 'react-icons/ri';
+import { HiOutlineEye } from 'react-icons/hi';
 
 type PropsType = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function ButtonEdit({ ...attribute }: PropsType) {
+export default function ButtonWatch({ ...attribute }: PropsType) {
   return (
     <button
-      title="Edit"
+      title="Watch"
       {...attribute}
-      className="flex items-center justify-center text-md h-8 w-8 rounded-md text-orenge bg-orenge/10 cursor-pointer hover:bg-orenge/20"
+      className="flex items-center justify-center text-md h-8 w-8 rounded-md text-green bg-green/10 cursor-pointer hover:bg-green/20"
     >
-      <RiEditBoxLine
+      <HiOutlineEye
         className="text-xl"
       />
     </button>
